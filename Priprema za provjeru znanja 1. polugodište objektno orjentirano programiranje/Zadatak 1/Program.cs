d698bde055187547025d215308c2b766106ddc95
ClashMania <113007885+ClashMania@users.noreply.github.com>
2023-01-22 19:56:07 +0100

    Add files via upload

---
+++ Priprema za provjeru znanja 1. polugodište objektno orjentirano programiranje/Zadatak 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_1
+{
+    public class Fruit
+    {
+        private string Vrsta;
+        private string Boja;
+        private bool JeLiSlatko;
+        public Fruit(string s)
+        {
+            Vrsta = s;
+        }
+        public Fruit(string s1, string s2, bool b)
+        {
+            Vrsta = s1;
+            Boja = s2;
+            JeLiSlatko = b;
+        }
+    }
+    public class Test
+    {
+        public static void Main()
+        {
+            Fruit x = new Fruit("Jagoda", "Crvena", true);
+            Fruit y = new Fruit("Limun", "Žuta", false);
+            Fruit z = new Fruit("Avokado");
+        }
+    }
+}
---
+++ Priprema za provjeru znanja 1. polugodište objektno orjentirano programiranje/Zadatak 2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadatak_2
+{
+    class GeometrijskiLik
+    {
+        protected byte BrojStranica;
+        protected double[] Stranice = new double[10];
+        public void UcitajStranice()
+        {
+            for (int i = 0; i < BrojStranica; i++)
+            {
+                Console.WriteLine("Upišite duljinu {0}. stranice", i + 1);
+                string s = Console.ReadLine();
+                Stranice[i] = Convert.ToDouble(s);
+            }
+        }
+        public double Opseg()
+        {
+            double opseg = 0;
+            for (int i = 0; i < BrojStranica; i++)
+                opseg += Stranice[i];
+            return opseg;
+        }
+    }
+    class Trokut : GeometrijskiLik
+    {
+        public Trokut()
+        {
+            BrojStranica = 3;
+        }
+    }
+    class Cetverokut : GeometrijskiLik
+    {
+        public Cetverokut()
+        {
+            BrojStranica = 4;
+        }
+    }
+    class Test
+    {
+        public static void Main()
+        {
+            Trokut t = new Trokut();
+            t.UcitajStranice();
+            Console.WriteLine("Opseg trokuta je {0}", t.Opseg());
+            Cetverokut c = new Cetverokut();
+            c.UcitajStranice();
+            Console.WriteLine("Opseg četverokuta je {0}", c.Opseg());
+        }
+    }
+}
